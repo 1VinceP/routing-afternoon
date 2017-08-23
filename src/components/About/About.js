66edc2a206741dc87c1874cc6ebebf1c5e367668
Vincent Palmer <vpalmergraphics@gmail.com>
2017-08-23 08:56:53 -0600

    finished!

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-export default class About extends Component {
-
-render() {
+export default function( props ) {
     return (
         <div>
-            <h1>About</h1>
+            <nav>
+                <Link to='/about/history' >History</Link>
+                <Link to='/about/contact' >Contact</Link>
+            </nav>
+            { props.children }
         </div>
     )
-}
-
-
     
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 export default class ClassList extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            students: []
+        }
+    }
 
+    componentDidMount() {
+        return axios.get('http://localhost:3005/students')
+                    .then( res => {
+                        console.log( res.data )
+                        this.setState({
+                            students: res.data
+                        })
+                    })
+    }
+
+    render() {
+        const students = this.state.students.map( ( student, i ) => (
+            <div key={i}>
+                <Link to={`/student/${student.id}`}><h3>{ student.first_name } { student.last_name }</h3></Link>
+            </div>
+        ))
+
+        return(
+            <div>
+                <h1>ClassList: </h1>
+                { students }
+            </div>
+        )
+    }
 
 
 
